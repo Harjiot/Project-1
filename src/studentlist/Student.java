19c7e8209d8f28e608d76453a1302a633fef3f12
GURMAN <GURMAN@10.48.40.173>
2019-02-05 09:57:30 -0500

    maxHours with getter and setter

---
+++ src/studentlist/Student.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package studentlist;
+
+/**
+ *
+ * @author GURMAN
+ */
+public class Student {
+ private String name;
+    private int ID;
+    private maxHours;
+    public Student()
+    {
+        name = "Name Not Set";
+        ID = 0;
+    }
+    public Student(String givenName, int givenID)
+    {
+          this.name = givenName;
+          this.ID = givenID;
+    }
+    public void setName(String givenName)
+    {
+       this.name = givenName;
+    }
+    
+    public String getName()
+    {
+           return name;
+    }
+    
+    public void setID (int givenID)
+    {
+        this.ID = givenID;
+    }
+    
+    public int getID ()
+    {   
+        return ID;
+    }
+}
