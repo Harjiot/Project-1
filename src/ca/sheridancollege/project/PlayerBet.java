3c15eca0da6bdef9589c5b9e2c942b0f9d3442ef
TheCodeBreaker <scorch45@hotmail.com>
2019-04-01 22:44:41 -0400

    Project Blackjack

---
+++ src/ca/sheridancollege/project/PlayerBet.java
@@ -0,0 +1,19 @@
+package ca.sheridancollege.project;
+
+public class PlayerBet {
+
+	private double bet;
+
+	public double getBet() {
+		return this.bet;
+	}
+
+	/**
+	 * 
+	 * @param bet
+	 */
+	public void setBet(double bet) {
+		this.bet = bet;
+	}
+
+}
---
+++ src/ca/sheridancollege/project/RestartGame.java
@@ -0,0 +1,10 @@
+package ca.sheridancollege.project;
+
+public class RestartGame {
+
+	public void playAgain() {
+		// TODO - implement RestartGame.playAgain
+		throw new UnsupportedOperationException();
+	}
+
+}
